e2fddd596db177bd9a1ce799af8446fe4eb072cb
Ayush-Asutkar <asutkarayush@gmail.com>
2023-09-19 16:17:48 +0530

    Added functionality to print transitions

--- src/grammar/LR0Grammar.java
+++ src/grammar/LR0Grammar.java
@@ -88,4 +88,76 @@ public class LR0Grammar extends Grammar {
             }
         }
     }
+
+    private Map<State, Integer> indexingStates(){
+        Map<State, Integer> result = new HashMap<>();
+        int ind = 0;
+
+        for(Map.Entry<State, Map<String, State>> mapElem: this.transitions.entrySet()) {
+            State fromState = mapElem.getKey();
+            if (!result.containsKey(fromState)) {
+                result.put(fromState, ind++);
+            }
+
+            for (Map.Entry<String, State> mapValueElem: mapElem.getValue().entrySet()) {
+                State toState = mapValueElem.getValue();
+                if(!result.containsKey(toState)) {
+                    result.put(toState, ind++);
+                }
+            }
+        }
+
+        System.out.println("Total States: " + result.size());
+        System.out.println("Indexing of maps:");
+        for(Map.Entry<State, Integer> mapElem: result.entrySet()) {
+            System.out.println(mapElem.getValue() + " :-\n" + mapElem.getKey());
+        }
+
+        return result;
+    }
+
+    public void printTransitions() {
+        Map<State, Integer> stateIndexing = this.indexingStates();
+
+        for (Map.Entry<State, Map<String, State>> mapElem: this.transitions.entrySet()) {
+            int fromState = stateIndexing.get(mapElem.getKey());
+            Map<String, State> mapValues = mapElem.getValue();
+
+            for(Map.Entry<String, State> transitionState: mapValues.entrySet()) {
+                String transitionString = transitionState.getKey();
+                int toState = stateIndexing.get(transitionState.getValue());
+
+                System.out.println("From State: " + fromState);
+                System.out.println("Transition String: " + transitionString);
+                System.out.println("To State: " + toState);
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+    }
+
+    //for testing
+    public static void main(String[] args) {
+        LR0Grammar grammar = new LR0Grammar();
+        grammar.setFirstSymbol("E");
+        grammar.addTerminalSymbol("+");
+        grammar.addTerminalSymbol("*");
+        grammar.addTerminalSymbol("(");
+        grammar.addTerminalSymbol(")");
+        grammar.addTerminalSymbol("id");
+
+        grammar.addNonTerminalSymbol("E");
+        grammar.addNonTerminalSymbol("T");
+        grammar.addNonTerminalSymbol("F");
+
+        grammar.addRule("E -> E + T | T");
+        grammar.addRule("T -> T * F | F");
+        grammar.addRule("F -> ( E ) | id");
+
+        grammar.printGrammar();
+
+        grammar.computeTransitions();
+        grammar.printTransitions();
+    }
 }
